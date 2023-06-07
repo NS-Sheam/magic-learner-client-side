e30ba0eec7e1c8b36f8e13f4fbb3e1daf7fe5f00
NS-Sheam <123sheamfeni@gmail.com>
2023-06-07 13:15:07 +0600

    Auth provider initial setup done

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,22 @@
+import { getAuth } from "firebase/auth";
+import { createContext, useState } from "react";
+import app from "../Firebase/firebase.config";
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    setUser("Karim")
+
+    const authInfo = {
+        user
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
