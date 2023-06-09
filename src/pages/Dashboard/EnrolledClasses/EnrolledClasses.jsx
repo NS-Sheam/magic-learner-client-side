3c599d1689e544ecf5901d9698f3bf4da85fa5f8
NS-Sheam <123sheamfeni@gmail.com>
2023-06-09 23:23:59 +0600

    Enrolled class data fetching done

--- src/pages/Dashboard/EnrolledClasses/EnrolledClasses.jsx
+++ src/pages/Dashboard/EnrolledClasses/EnrolledClasses.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const EnrolledClasses = () => {
+    const {user, loading} = useAuth();
+    const [enrollClass, setEnrollClass] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/users?email=${user?.email}`)
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+        })
+    }, [])
     return (
         <div>
             <SectionTitle heading={"Enrolled Classes"}
