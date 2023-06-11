ed71b46255c0e312add4696b1efcb24f341bd413
NS-Sheam <123sheamfeni@gmail.com>
2023-06-11 22:17:56 +0600

    banner responsive done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -7,24 +7,24 @@ import img3 from "../../../assets/banner-image3.jpg"
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Banner = () => {
-    const bannerLeft = (img) => <div className="hero h-[700px]">
+    const bannerLeft = (img) => <div className="lg:h-[700px]  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
         {/* TODO: make small banner for mobile devices */}
-        <div className="hero-content text-neutral-content">
+        <div className="lg:flex lg:flex-row justify-between items-center text-center text-neutral-content p-4 space-y-4">
             <div
-                className="my-container w-1/2 space-y-4 text-left"
+                className="my-container lg:w-1/2 space-y-4 text-center lg:text-left"
                 data-aos="fade-right"
                 data-aos-anchor-placement="top-bottom"
                 data-aos-duration="1000"
             >
-                <h3 className="lg:text-6xl text-bandOrange font-bold">Start Music School Summer Camp!</h3>
-                <p>Discover the rhythm of summer at our Music School Summer Camp! Unleash your musical potential, make new friends, and have a blast with instrument lessons, ensemble rehearsals, and exciting activities. Join us for an unforgettable musical journey</p>
+                <h3 className="text-2xl lg:text-6xl text-bandOrange font-bold">Start Music School Summer Camp!</h3>
+                <p className="text-xl font-semibold">Discover the rhythm of summer at our Music School Summer Camp! Unleash your musical potential, make new friends, and have a blast with instrument lessons, ensemble rehearsals, and exciting activities. Join us for an unforgettable musical journey</p>
                 <button className="text-xl px-6 py-3 bg-bandOrange hover:bg-orange-300 hover:text-black font-bold rounded-lg">Enroll now!</button>
             </div>
             <div
                 data-aos="fade-left"
                 data-aos-anchor-placement="top-bottom"
                 data-aos-duration="1000"
-                className="w-1/2 rounded-3xl">
+                className="lg:w-1/2 rounded-3xl">
                 <img className="rounded-3xl" src={img} alt="" />
             </div>
         </div>
