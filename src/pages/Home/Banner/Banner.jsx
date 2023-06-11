6916cabc23c1133cd9e1a557ea7a1d72cf6215b3
NS-Sheam <123sheamfeni@gmail.com>
2023-06-11 22:41:40 +0600

    menu button done for mobile

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,6 +6,7 @@ import img2 from "../../../assets/banner-image2.jpg"
 import img3 from "../../../assets/banner-image3.jpg"
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Link } from "react-router-dom";
 const Banner = () => {
     const bannerLeft = (img) => <div className="lg:h-[700px]  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
         {/* TODO: make small banner for mobile devices */}
@@ -18,7 +19,9 @@ const Banner = () => {
             >
                 <h3 className="text-2xl lg:text-6xl text-bandOrange font-bold">Start Music School Summer Camp!</h3>
                 <p className="text-xl font-semibold">Discover the rhythm of summer at our Music School Summer Camp! Unleash your musical potential, make new friends, and have a blast with instrument lessons, ensemble rehearsals, and exciting activities. Join us for an unforgettable musical journey</p>
-                <button className="text-xl px-6 py-3 bg-bandOrange hover:bg-orange-300 hover:text-black font-bold rounded-lg">Enroll now!</button>
+                <Link to="/allclass">
+                    <button className="text-xl px-6 py-3 bg-bandOrange hover:bg-orange-300 hover:text-black font-bold rounded-lg">Enroll now!</button>
+                </Link>
             </div>
             <div
                 data-aos="fade-left"
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -6,14 +6,14 @@ import { AuthContext } from '../../../Providers/AuthProvider';
 import ActiveLink from './ActiveLink';
 import { Link } from 'react-router-dom';
 const Header = () => {
-    const {user, logOut} = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
     // console.log(user);
-    const handleLogOut = () =>{
+    const handleLogOut = () => {
         logOut()
-        .then()
-        .catch(error => {
-            console.log(error);
-        })
+            .then()
+            .catch(error => {
+                console.log(error);
+            })
     }
     const menuItems = <>
         <li>
@@ -41,18 +41,6 @@ const Header = () => {
     return (
         <div className="navbar bg-orange-primary text-black font-bold lg:py-6">
             <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="bg-orange-primary menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52">
-                        {
-                            menuItems
-                        }
-                    </ul>
-                </div>
-
-
                 <Link to="/" className="flex justify-center items-center lg:gap-4">
                     <img className=" h-6 lg:h-10" src={logo} alt="logo" />
                     <h3 className="lg:text-xl font-bold">Magic Learner</h3>
@@ -94,6 +82,16 @@ const Header = () => {
                     }
                 </ul>
             </div>
+            <div className="dropdown navbar-end lg:hidden me-4">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden w-full flex items-center justify-end">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="bg-white menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 z-10 right-6">
+                        {
+                            menuItems
+                        }
+                    </ul>
+                </div>
         </div>
     );
 };
