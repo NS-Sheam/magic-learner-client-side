c67fc8d2578b47cb1fab85bedf76ee1a15ca4187
NS-Sheam <123sheamfeni@gmail.com>
2023-06-12 12:56:05 +0600

    Framer motion animation done

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,15 +1,30 @@
+import { motion } from "framer-motion";
+import logo from "../assets/logo.png";
 
-import logo from "../assets/logo.png"
 const SectionTitle = ({ heading, description }) => {
-    const head = heading.split(' ');
-    // console.log(head);
-    return (
-        <div className="text-center lg:my-8 space-y-4 mx-auto">
-            <h2 className="lg:text-3xl font-bold text-bandOrange uppercase">{heading}</h2>
-            <img className="h-10 w-10 mx-auto" src={logo} alt="" />
-            <p className="lg:w-2/3 mx-auto">{description}</p>
-        </div>
-    );
+  // console.log(head);
+  return (
+    <div className="text-center lg:my-8 space-y-4 mx-auto">
+      <h2 className="lg:text-3xl font-bold text-bandOrange uppercase">{heading}</h2>
+      <motion.img
+        className="h-10 w-10 mx-auto"
+        src={logo}
+        alt=""
+        animate={{
+          scale: [1, 1.2, 1.2, 1, 1],
+          rotate: [0, 0, 360, 360, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+          repeat: Infinity,
+          repeatDelay: 1,
+        }}
+      />
+      <p className="lg:w-2/3 mx-auto">{description}</p>
+    </div>
+  );
 };
 
 export default SectionTitle;
--- src/pages/Home/GurdianMessage/GurdianMessage.jsx
+++ src/pages/Home/GurdianMessage/GurdianMessage.jsx
@@ -7,7 +7,7 @@ const GurdianMessage = () => {
     const feedBackCard = <div className="bg-black bg-opacity-10 border text-white shadow-white border-white rounded-lg shadow p-4">
         <div className="flex items-center mb-4r relative">
             <img
-                className="w-10 h-10 rounded-full absolute -right-8 -top-8"
+                className="w-10 h-10 lg:w-14 lg:h-14 rounded-full absolute -right-8 lg:-right-10 -top-8 lg:-top-10"
                 src="https://i.ibb.co/Dk08kh9/chef5.jpg" // Replace with customer's profile image URL
                 alt="Customer Profile"
             />
@@ -17,7 +17,7 @@ const GurdianMessage = () => {
             </div>
         </div>
         <p className="">
-            Summer Camp Music School exceeded our expectations! Our child had an amazing time learning and exploring different instruments. The instructors were skilled and passionate, creating a positive and supportive environment. We were impressed with the variety of classes offered and the progress our child made in such a short time. We highly recommend Summer Camp Music School to any parent looking to foster their child musical talents and create lasting memories.
+            Summer Camp Music School exceeded our expectations! Our child had an amazing time learning and exploring different instruments. The instructors were skilled and passionate, creating a positive and supportive environment. We were impressed with the variety of classes offered and the progress our child made in such a short time. 
         </p>
     </div>
     const repeatCards = [];
@@ -29,9 +29,9 @@ const GurdianMessage = () => {
 
             className="gurdian-message-bg my-container py-8 lg:py-14 min-h-screen bg-cover bg-fixed">
             <div className="text-center lg:my-8 space-y-4 mx-auto">
-                <h2 className="lg:text-3xl font-bold text-white uppercase">Gurdians Feedback</h2>
+                <h2 className="lg:text-3xl font-bold text-white uppercase">Gurdians Message</h2>
                 <img className="h-10 w-10 mx-auto" src={logo} alt="" />
-                <p className="lg:w-2/3 mx-auto text-white">Here is some gurdian feedback about our classes</p>
+                <p className="lg:w-2/3 mx-auto text-white">Discover what guardians are saying about us in the feedback section. Read firsthand experiences, testimonials, and suggestions that highlight the positive impact of our music school. Join the conversation and share your feedback to help us provide exceptional musical education.</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {/* Feedback Card */}
