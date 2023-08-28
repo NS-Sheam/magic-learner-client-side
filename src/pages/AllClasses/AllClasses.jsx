1b636e4294b8586d1fa8aa464ed037bc53844dfe
NS-Sheam <123sheamfeni@gmail.com>
2023-08-28 15:21:30 +0600

    card modify by theme and disable banner swipe

--- src/pages/AllClasses/AllClasses.jsx
+++ src/pages/AllClasses/AllClasses.jsx
@@ -108,11 +108,7 @@ const AllClasses = () => {
                 }
             })
     }
-    if (loading || isAdminLoading || classLoading) {
-        return <div className='h-screen flex justify-center items-center'>
-            <span className="loading loading-bars loading-lg"></span>
-        </div>
-    }
+
     return (
         <div className="">
             <SectionTitle
